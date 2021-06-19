717742687e7c3f48c7584d069283588c89b9523e
Simtrami <clement.taverne@telecom-sudparis.eu>
2021-06-19 17:02:32 +0200

    Minor fixes
    Signed-off-by: Clément T <clement.taverne@telecom-sudparis.eu>

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -2,9 +2,9 @@
     <thead>
     <tr class="rounded-md bg-theme text-white">
         @forelse($columns as $colDisplay => $col)
-            <th class="border-b-2 border-theme-dark border-opacity-75 p-1.5">{{ $colDisplay }}</th>
+            <th class="border-b-2 border-theme-light p-1.5">{{ $colDisplay }}</th>
         @empty
-            <th class="border-b-2 border-theme-dark border-opacity-75 p-1.5">{!! __('table.no_column') !!}</th>
+            <th class="border-b-2 border-theme-dark p-1.5">{!! __('table.no_column') !!}</th>
         @endforelse
     </tr>
     </thead>
@@ -12,7 +12,7 @@
     @forelse ($elements as $elt)
         <tr class="hover:bg-gray-50 dark:hover:bg-dark">
             @foreach($columns as $col)
-                <td class="border-t-2 border-dotted border-theme-dark border-opacity-75 p-1.5">{{ $elt[$col] }}</td>
+                <td class="border-t-2 border-dotted border-theme-dark p-1.5">{{ $elt[$col] }}</td>
             @endforeach
         </tr>
     @empty

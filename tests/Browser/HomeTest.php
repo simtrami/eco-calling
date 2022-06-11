9145a5e66ebe98bc0875f3360420d3aae984ee29
Simtrami <me@simtrami.net>
2022-06-11 15:48:25 +0200

    Localize browser tests
    Signed-off-by: Simtrami <me@simtrami.net>

--- tests/Browser/HomeTest.php
+++ tests/Browser/HomeTest.php
@@ -6,9 +6,15 @@ use Laravel\Dusk\Browser;
 
 uses(DatabaseMigrations::class);
 
+beforeEach(function () {
+    App::setLocale('fr');
+});
+
 it('has a manifesto page', function () {
     $this->browse(function (Browser $browser) {
         $browser->visit('/')
-            ->assertSourceHas(__('index.title'));
+            ->assertSourceHas(__('index.title', locale: 'fr'));
+        $browser->visit('/en')
+            ->assertSourceHas(__('index.title', locale: 'en'));
     });
 });
--- tests/Browser/SignaturesTest.php
+++ tests/Browser/SignaturesTest.php
@@ -5,14 +5,23 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\WithFaker;
 use Laravel\Dusk\Browser;
 
-uses(DatabaseMigrations::class);
-uses(WithFaker::class);
+uses(
+    DatabaseMigrations::class,
+    WithFaker::class
+);
+
+beforeEach(function () {
+    App::setLocale('fr');
+});
 
 it('has a signature page without signatures', function () {
     $this->browse(function (Browser $browser) {
         $browser->visit('/signatures')
-            ->assertSee(__('signatures.table.empty'))
-            ->assertSourceMissing(__('pagination.nav_label'));
+            ->assertSee(__('signatures.table.empty', locale: 'fr'))
+            ->assertSourceMissing(__('pagination.nav_label', locale: 'fr'));
+        $browser->visit('/en/signatures')
+            ->assertSee(__('signatures.table.empty', locale: 'en'))
+            ->assertSourceMissing(__('pagination.nav_label', locale: 'en'));
     });
 });
 it('has one signature page with 10 signatures', function () {
@@ -20,15 +29,26 @@ it('has one signature page with 10 signatures', function () {
     $this->browse(function (Browser $browser) {
         $browser->visit('/signatures')
             ->assertSee(Signature::confirmed()->first()->full_name)
-            ->assertSourceMissing(__('pagination.nav_label'));
+            ->assertSourceMissing(__('pagination.nav_label', locale: 'fr'));
+        $browser->visit('/en/signatures')
+            ->assertSee(Signature::confirmed()->first()->full_name)
+            ->assertSourceMissing(__('pagination.nav_label', locale: 'en'));
     });
 });
 it('has a first signature page with 20 signatures on two pages', function () {
     Signature::factory()->confirmed()->count(20)->create();
     $this->browse(function (Browser $browser) {
         $browser->visit('/signatures?page=1')
-            ->assertSee("De 1 à 10 sur 20 résultats")
-            ->assertSourceHas(__('pagination.nav_label'))
+            // "De 1 à 10 sur 20 résultats"
+            ->assertSee(__('pagination.showing', locale: 'fr') . ' 1 ' . __('pagination.to', locale: 'fr') . ' 10 ' . __('pagination.of', locale: 'fr') . ' 20 ' . __('pagination.results', locale: 'fr'))
+            ->assertSourceHas(__('pagination.nav_label', locale: 'fr'))
+            ->assertSourceHas('<span class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-white bg-theme border border-theme cursor-default leading-5">1</span>');
+    });
+    $this->browse(function (Browser $browser) {
+        $browser->visit('/en/signatures?page=1')
+            // "Showing 1 to 10 of 20 results"
+            ->assertSee(__('pagination.showing', locale: 'en') . ' 1 ' . __('pagination.to', locale: 'en') . ' 10 ' . __('pagination.of', locale: 'en') . ' 20 ' . __('pagination.results', locale: 'en'))
+            ->assertSourceHas(__('pagination.nav_label', locale: 'en'))
             ->assertSourceHas('<span class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-white bg-theme border border-theme cursor-default leading-5">1</span>');
     });
 });
@@ -36,8 +56,16 @@ it('has a second signature page with 20 signatures on two pages', function () {
     Signature::factory()->confirmed()->count(20)->create();
     $this->browse(function (Browser $browser) {
         $browser->visit('/signatures?page=2')
-            ->assertSee("De 11 à 20 sur 20 résultats")
-            ->assertSourceHas(__('pagination.nav_label'))
+            // "De 11 à 20 sur 20 résultats"
+            ->assertSee(__('pagination.showing', locale: 'fr') . ' 11 ' . __('pagination.to', locale: 'fr') . ' 20 ' . __('pagination.of', locale: 'fr') . ' 20 ' . __('pagination.results', locale: 'fr'))
+            ->assertSourceHas(__('pagination.nav_label', locale: 'fr'))
+            ->assertSourceHas('<span class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-white bg-theme border border-theme cursor-default leading-5">2</span>');
+    });
+    $this->browse(function (Browser $browser) {
+        $browser->visit('/en/signatures?page=2')
+            // "Showing 11 to 20 of 20 results"
+            ->assertSee(__('pagination.showing', locale: 'en') . ' 11 ' . __('pagination.to', locale: 'en') . ' 20 ' . __('pagination.of', locale: 'en') . ' 20 ' . __('pagination.results', locale: 'en'))
+            ->assertSourceHas(__('pagination.nav_label', locale: 'en'))
             ->assertSourceHas('<span class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-white bg-theme border border-theme cursor-default leading-5">2</span>');
     });
 });
@@ -45,9 +73,18 @@ it('does not have a third signature page with 20 signatures on two pages', funct
     Signature::factory()->confirmed()->count(20)->create();
     $this->browse(function (Browser $browser) {
         $browser->visit('/signatures?page=3')
-            ->assertSee(__('signatures.table.out_of_range'))
-            ->assertSee("De à sur 20 résultats")
-            ->assertSourceHas(__('pagination.nav_label'))
+            ->assertSee(__('signatures.table.out_of_range', locale: 'fr'))
+            // "De à sur 20 résultats"
+            ->assertSee(__('pagination.showing', locale: 'fr') . ' ' . __('pagination.to', locale: 'fr') . ' ' . __('pagination.of', locale: 'fr') . ' 20 ' . __('pagination.results', locale: 'fr'))
+            ->assertSourceHas(__('pagination.nav_label', locale: 'fr'))
+            ->assertSourceMissing('<span class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-white bg-theme border border-theme cursor-default leading-5">3</span>');
+    });
+    $this->browse(function (Browser $browser) {
+        $browser->visit('/en/signatures?page=3')
+            ->assertSee(__('signatures.table.out_of_range', locale: 'en'))
+            // "Showing to of 20 results"
+            ->assertSee(__('pagination.showing', locale: 'en') . ' ' . __('pagination.to', locale: 'en') . ' ' . __('pagination.of', locale: 'en') . ' 20 ' . __('pagination.results', locale: 'en'))
+            ->assertSourceHas(__('pagination.nav_label', locale: 'en'))
             ->assertSourceMissing('<span class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-white bg-theme border border-theme cursor-default leading-5">3</span>');
     });
 });
